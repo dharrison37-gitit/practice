c0b11f3e90bce7610828c53d90dc18d64a5541ba
dharrison37-gitit <308913916+dharrison37-gitit@users.noreply.github.com>
2026-09-21 15:11:44 -0500

    Update webpack imports

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,5 @@
 import { merge } from "webpack-merge";
 import common from "./webpack.common.js";
-import HtmlWebpackPlugin from "html-webpack-plugin";
 
 export default merge(common, {
     mode: "development",
